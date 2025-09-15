d24ea6098da60b87162f95fed879aca2ed80f5e4
Tony Brierly <anthonylbrierly@gmail.com>
2025-09-15 13:57:15 -0700

    Update database to Postgres from MySql

--- src/components/CalendifyLogo.tsx
+++ src/components/CalendifyLogo.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { RiCalendarScheduleLine } from "react-icons/ri";
 
 const CalendifyLogo = () => {
   return (
     <div className="flex items-center gap-x-2 bg-white/10 px-3 py-1 rounded-lg shadow-sm cursor-default">
-      <RiCalendarScheduleLine className="w-8 h-8" />
-      <span className="text-3xl font-semibold ">Calendify</span>
+      <span className="text-3xl font-semibold "> 📆 Calendify</span>
     </div>
   );
 };
